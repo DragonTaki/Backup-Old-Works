abbd92e4ee45aecd25d22ad3879be2e141aae5f7
Dragon Taki <synasaivaltos@gmail.com>
2025-03-19 12:55:28 +0800

    Add files via upload

---
+++ (Example).cpp
@@ -0,0 +1,36 @@
+/*----- ----- ----- ----- ----- -----*/
+//             (Example)             //
+//                                   //
+//Auther : Synasaivaltos Terdolic    //
+//Company:                           //
+//Date   : 2015/  /                  //
+//                                   //
+//      synasaivaltos@gmail.com      //
+/*----- ----- ----- ----- ----- -----*/
+
+#include <conio.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void pause(void);
+
+int main(void)
+{
+	while(1)
+	{
+		
+	}
+	
+	pause();
+	return 0;
+}
+
+void pause(void)
+{
+	cout << "Press any key to continue...";
+	char temp=getch();
+	
+	return;
+}
